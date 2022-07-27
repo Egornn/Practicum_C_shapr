4c12bee8fe1ab6f149fc67e012e9a40433d411b8
Egor Nazdryuhin <egornnn@gmail.com>
2022-07-27 18:25:20 +0300

    method for the array printing

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿void PrintArray (string [] input)
+{
+    for (int i = 0; i < input.Length - 1;i++)
+    {
+        System.Console.Write($"{input[i]}, ");
+    }
+    System.Console.WriteLine($"{input[input.Length-1]}.");
+}
